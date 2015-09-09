c995223d3acaf9f517d6834da33c593876117daf
Loretta <loretta.he@swiftkey.net>
2015-09-09 09:40:38 +0100

    Fixing auto-complete problem on ipython console

--- src/ipython_clojure/messaging/auto_complete.clj
+++ src/ipython_clojure/messaging/auto_complete.clj
@@ -7,8 +7,10 @@
 (defmethod reply-to-message "complete_request"
   [message shell-socket iopub-socket repl-client]
   (let [code (get-in message [:content :code])
+        _ (println code)
         cursor-pos (get-in message [:content :cursor_pos])
-        code-up-to-cursor (subs code 0 (inc cursor-pos))
+        _ (println cursor-pos)
+        code-up-to-cursor (subs code 0 cursor-pos)
         last-space (.lastIndexOf code-up-to-cursor " ")
         last-non-space-symbol (if (>= last-space 0) (subs code-up-to-cursor (inc last-space)) code-up-to-cursor)
         actual-symbol (clojure.string/replace last-non-space-symbol #"[(\[{,}\])]+" "")
