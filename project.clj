823b365b043d9ddcd2f7b041eecd972f06fa0b5a
Loretta <loretta.he@swiftkey.net>
2015-09-09 16:55:44 +0100

    Increment verison before release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ipython-clojure "0.1.0-SNAPSHOT"
+(defproject ipython-clojure "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
