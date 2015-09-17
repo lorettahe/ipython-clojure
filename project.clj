588c541dd780b994d2b2ae78b35a11463fd12bdf
Loretta <loretta.he@swiftkey.net>
2015-09-17 11:16:21 +0100

    Increasing version to stable before release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ipython-clojure "0.1.2-SNAPSHOT"
+(defproject ipython-clojure "0.1.2"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
